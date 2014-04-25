ad4bb59b7ac5c2c6ecf7013d058f306245a2fe10
SeedPaths-User <lessdismissive@email.com>
2014-04-25 14:08:53 -0600

    op
    object practise

---
+++ objectpractise/objectpractise/weapons.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objectpractise
+{
+    class weapons
+    {
+        private string Name;
+       private string firePower;
+       private string type;
+       private int speed;
+
+        static void weapons (string weaponName)
+        {
+              weapons AK47 = new weapons();
+            weapons ZH29  = new weapons();
+            weapons Vz24= new weapons();
+        }
+    }
+        
+}
